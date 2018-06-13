d067a435c70f23b70fbde84e854ff61f7d157543
avi.home <avi.upsale@gmail.com>
2018-06-13 23:46:05 +0300

    spark job upd

--- spark1.py
+++ spark1.py
@@ -3,9 +3,16 @@ from pyspark import SparkContext as sc
 from pyspark.sql.functions import concat_ws
 import pyspark
 import sys
+import requests
+import json
 # sys.setdefaultencoding() does not exist, here!
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
+
+r = requests.get('http://0.0.0.0:5000/jsonresult')
+with open('/root/rezerv/output.json', 'w') as outfile:
+    json.dump(r.json(), outfile)
+
 sqlContext = pyspark.SQLContext(pyspark.SparkContext())
 df = sqlContext.read.json('file:///root/rezerv/output.json')
 df.select('geometry.location.lat', 'geometry.location.lng', 'name', concat_ws(',', 'types'), 'rating', 'vicinity').write.csv('/datastorage/poidata.csv')
